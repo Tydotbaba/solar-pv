3e67e51afb9d49dc4db6c9980c0aa766a9b79dbe
Tydotbaba <oyes2011@gmail.com>
2019-07-10 12:41:37 +0100

    Updates

---
+++ precache-manifest.bc28e05a569aa3ec78eb54ec40a772b9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fcf69e5359987c9b1545aae1c2c5faa",
+    "url": "/solar-pv/static/media/emilyz.9fcf69e5.jpg"
+  },
+  {
+    "revision": "dac8c44acc9d08218e1f",
+    "url": "/solar-pv/static/css/main.fcd4d56d.chunk.css"
+  },
+  {
+    "revision": "93eabffe93d32a5b43e9",
+    "url": "/solar-pv/static/js/1.93eabffe.chunk.js"
+  },
+  {
+    "revision": "f867e78020950cdd9410",
+    "url": "/solar-pv/static/js/runtime~main.f867e780.js"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/solar-pv/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "eb6be414b75c412c13eeee2d2e8390cb",
+    "url": "/solar-pv/static/media/react-logo.eb6be414.png"
+  },
+  {
+    "revision": "dac8c44acc9d08218e1f",
+    "url": "/solar-pv/static/js/main.dac8c44a.chunk.js"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/solar-pv/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/solar-pv/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/solar-pv/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/solar-pv/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "4ce876de30f00627fb80f07a10e25d2e",
+    "url": "/solar-pv/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Tydotbaba/solar-pv/precache-manifest.3f24f41385342d9ad1ecc7672e894964.js"
+  "/solar-pv/precache-manifest.bc28e05a569aa3ec78eb54ec40a772b9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Tydotbaba/solar-pv/index.html", {
+workbox.routing.registerNavigationRoute("/solar-pv/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
