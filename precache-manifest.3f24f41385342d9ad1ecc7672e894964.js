78abf2ede3ba1fef07a37381d20150782d21c133
Tydotbaba <oyes2011@gmail.com>
2019-07-10 02:17:28 +0100

    Updates

---
+++ precache-manifest.3f24f41385342d9ad1ecc7672e894964.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fcf69e5359987c9b1545aae1c2c5faa",
+    "url": "/Tydotbaba/solar-pv/static/media/emilyz.9fcf69e5.jpg"
+  },
+  {
+    "revision": "b95fb122ab11b36a5423",
+    "url": "/Tydotbaba/solar-pv/static/css/main.d4822dfd.chunk.css"
+  },
+  {
+    "revision": "af772a0e90ba0a4eac5e",
+    "url": "/Tydotbaba/solar-pv/static/js/1.af772a0e.chunk.js"
+  },
+  {
+    "revision": "cdd6080f262aac8c75a1",
+    "url": "/Tydotbaba/solar-pv/static/js/runtime~main.cdd6080f.js"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/Tydotbaba/solar-pv/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "eb6be414b75c412c13eeee2d2e8390cb",
+    "url": "/Tydotbaba/solar-pv/static/media/react-logo.eb6be414.png"
+  },
+  {
+    "revision": "b95fb122ab11b36a5423",
+    "url": "/Tydotbaba/solar-pv/static/js/main.b95fb122.chunk.js"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/Tydotbaba/solar-pv/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/Tydotbaba/solar-pv/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/Tydotbaba/solar-pv/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/Tydotbaba/solar-pv/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "0db839ba15bb990a9aa109e74dc49569",
+    "url": "/Tydotbaba/solar-pv/index.html"
+  }
+];
